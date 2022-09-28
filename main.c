3d80979b6fc64e0764b4365b765c2cb26fa5e57e
root <root@ip-172-31-6-4.ec2.internal>
2022-09-28 09:58:39 +0000

    This changed done by developer1

--- main.c
+++ main.c
@@ -3,12 +3,17 @@ pseudo code
 Initial Function()
 {
   //Sample Code
+This code is changed by developer1
+
 }
 
 Initial Security()
 {
  //Sample Code
+This code is changed by developer1
 }
+
+
 
 
 
